76470303a72f8ca5c08f78b2f881deb2408880b1
Travace Reese <tsuk13@gmail.com>
2012-11-01 22:16:43 -0400

    implemented remove

--- BST.cpp
+++ BST.cpp
@@ -36,8 +36,44 @@ void BST<T>::insert(T v) {
 
 template <typename T>
 void BST<T>::remove(T v) {
-  Node<T>* temp = new Node<T>(v);
-  root = temp;
+  //find the nodeToRemove
+  Node<T>** nodeToRemove = &(root);
+  while((*nodeToRemove) != 0 && (*nodeToRemove)->getValue() != v){
+    if(v < (*nodeToRemove)->getValue())
+      nodeToRemove = &((*nodeToRemove)->getLeftChild());
+    else if(v > (*nodeToRemove)->getValue())
+      nodeToRemove = &((*nodeToRemove)->getRightChild());
+  }
+  //case nodetoRemove is not present
+  if((*nodeToRemove) == 0){
+    return;
+  }
+  //keep a pointer to nodeToRemove for deletion
+  Node<T>* tmp = (*nodeToRemove);
+  //case for no left child
+  if((*nodeToRemove)->getLeftChild() == 0){
+    (*nodeToRemove) = (*nodeToRemove)->getRightChild();
+    delete tmp;
+    return;
+  }
+  //case for no right child
+  else if((*nodeToRemove)->getRightChild() == 0){
+    (*nodeToRemove) = (*nodeToRemove)->getLeftChild();
+    delete tmp;
+    return;
+  }
+  //case two childs
+  //find in order predessesor
+  Node<T>* IOP = (*nodeToRemove)->getLeftChild();
+  while(IOP->getRightChild() != 0){
+    IOP = IOP->getRightChild();
+  }
+  //IOP's right subtree becomes what nodeToRemoves was
+  IOP->setRightChild(*((*nodeToRemove)->getRightChild()));
+  //nodeToRemove's parent pointer gets set to nodeToRemoves leftChild
+  (*nodeToRemove) = (*nodeToRemove)->getLeftChild();
+  //nodeToRemove is deleted
+  delete tmp;
 }
 
 template <typename T>
--- tree_test.cpp
+++ tree_test.cpp
@@ -3,14 +3,13 @@
 int main() {
   BST<int>* bst = new BST<int>();
 
-  bst->insert(5);
-  bst->insert(6);
-  bst->insert(3);
   bst->insert(2);
-  bst->insert(7);
-  //bst->insert(12);
-
-
+  bst->insert(1);
+  bst->insert(3);
+  bst->insert(5);
+  bst->insert(4);
+  bst->print();
+  bst->remove(3);
   bst->print();
 
 
